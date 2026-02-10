69cb38f3c2614b8d7b841817050db067599be4ec
LILKEVS\kevin <kevin.willis.andrew@gmail.com>
2026-02-09 17:40:59 -0800

    Work on character controller. Brought in steamworks samples.

---
+++ Assets/FishAndChips/Code/Opalite/OpaliteCharacterController.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+using Rewired;
+
+namespace FishAndChips
+{
+    public class OpaliteCharacterController : FishScript
+    {
+		#region -- Inspector --
+		public int PlayerId = 0;
+
+		public float XSensitivity = 1;
+		public float YSensitivity = 1;
+
+		public Transform Orientation;
+
+		public float CameraXRotation;
+		public float CameraYRotation;
+		#endregion
+
+		#region -- Private Member Vars --
+		private Player _rewiredPlayer;
+		private CharacterController _characterController;
+
+		private bool _interactionTriggered;
+		private Vector2 _mouseInputs = Vector2.zero;
+		#endregion
+
+		#region -- Private Methods --
+		private void Awake()
+		{
+			HandleCursor();
+			FetchPlayer();
+		}
+
+		private void Update()
+		{
+			if (ReInput.isReady == false)
+			{
+				return;
+			}
+			if (_rewiredPlayer == null)
+			{
+				FetchPlayer();
+			}
+			if (_rewiredPlayer == null)
+			{
+				return;
+			}
+
+			GetInput();
+			ProcessInput();
+		}
+
+		private void HandleCursor()
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+		}
+
+		private void FetchPlayer()
+		{
+			if (ReInput.isReady == true)
+			{
+				_rewiredPlayer = ReInput.players.GetPlayer(PlayerId);
+			}
+		}
+
+		private void GetInput()
+		{
+			_interactionTriggered = _rewiredPlayer.GetButtonDown("Interact");
+			_mouseInputs.x = _rewiredPlayer.GetAxis("HorizontalView");
+			_mouseInputs.y = _rewiredPlayer.GetAxis("VerticalView");
+		}
+
+		private void ProcessInput()
+		{
+			if (_interactionTriggered == true)
+			{
+				Debug.Log("Interaction Triggered");
+			}
+
+			CameraXRotation -= _mouseInputs.y * Time.deltaTime * XSensitivity;
+			CameraYRotation += _mouseInputs.x * Time.deltaTime * YSensitivity;
+
+			CameraXRotation = Mathf.Clamp(CameraXRotation, -90f, 90f);
+
+			transform.rotation = Quaternion.Euler(CameraXRotation, CameraYRotation,0);
+			Orientation.rotation = Quaternion.Euler(0, CameraYRotation, 0);
+		}
+		#endregion
+	}
+}
