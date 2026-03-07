22e45917782e2a9b82d81c5ea12672f22fabd4c2
LILKEVS\kevin <kevin.willis.andrew@gmail.com>
2026-03-07 07:30:07 -0800

    git attributes

--- Assets/FishAndChips/Code/Opalite/Level/LevelPuzzleVisual.cs
+++ Assets/FishAndChips/Code/Opalite/Level/LevelPuzzleVisual.cs
@@ -2,18 +2,10 @@ using UnityEngine;
 
 namespace FishAndChips
 {
-    public class LevelPuzzleVisual : MonoBehaviour
+    public class LevelPuzzleVisual : FishScript
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
-        
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
-    }
+		#region -- Inspector --
+		public GameObjectStateMachine StateMachine;
+		#endregion
+	}
 }
